f821282d3887f97df1dea907a48685776923232e
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-26 16:58:35 -0300

    Added eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: () => JSX.Element = () => {
+  const [count, setCount] = React.useState(0)
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => { setCount((count) => count + 1) }}>
           count is {count}
         </button>
         <p>
