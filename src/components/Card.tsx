cf4c96cddaabd90271921629955f1c4a1807f828
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-26 17:58:25 -0300

    Added Card texts

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,12 @@ const Card: () => JSX.Element = () => {
 
   return (
     <div className="card">
+      <div>
+        <h1>Upload your image</h1>
+        <p>File should be Jpeg, Png...</p>
+      </div>
       <ImageUploader />
+      <span>Or</span>
       <Button onClickHandler={handleClick}>
         Choose a file
       </Button>
