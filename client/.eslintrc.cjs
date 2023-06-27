723001ad2d38c75ba7c9ae8d41940d815addb62a
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-27 18:28:40 -0300

    Fixed wrong tsconfig path on eslintrc

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json']
+    project: ['./client/tsconfig.json']
   },
   plugins: [
     'react'
